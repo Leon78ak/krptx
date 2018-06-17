8f1eff88ca48a667a2679713784c4fe7ac81493b
Alexandr Kokorev <ak_78leon@mail.ru>
2018-06-17 13:18:40 +0600

    top-banner

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.burger');
+
+navMain.classList.remove('main-nav--no-js');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    var handler = function () {
+      alert(1);
+      navMain.style.display = 'block';
+      navMain.removeEventListener('transitionend', handler);
+    };
+    navMain.classList.remove('main-nav--closed');
+
+    navMain.classList.add('main-nav--opened');
+    navToggle.classList.add('burger--close');
+
+
+    navMain.addEventListener('transitionend', handler);
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navToggle.classList.remove('burger--close');
+  }
+});
 //for example
 window.onscroll = function () {
   var wScroll = window.pageYOffset;
